464b5c31f8697d89c697e730d9dea1158351bdb9
aadhyaputtur <putturaadhya@gmail.com>
2022-03-24 18:04:30 -0400

    Added Parser class that makes hashmap of nodeIds and location objects and parses through CSV file. Changed gradle file.

---
+++ src/main/java/edu/wpi/agileAngels/Parser.java
@@ -0,0 +1,76 @@
+package edu.wpi.agileAngels;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+public class Parser {
+
+    //Loop through each row
+        //Loop through each item of row
+    private static final String CSV_FILE_PATH = "./TowerLocations.csv";
+    private String[] headers = {"NodeID", "xcoord", "ycoord",  "floor", "building", "nodeType", "longName", "shortName"};
+    public HashMap<String, Location> locationData;
+    /**
+     * Reading from CSV file and creating a database and making Location
+     * @param connection
+     */
+    public void createTable(Connection connection) {//TODO make connection a public variable
+
+        try (Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
+             CSVParser csvParser =
+                     new CSVParser(
+                             reader,
+                             CSVFormat.DEFAULT
+                                     .withHeader(
+                                             "NodeID",
+                                             "xcoord",
+                                             "ycoord",
+                                             "floor",
+                                             "building",
+                                             "nodeType",
+                                             "longName",
+                                             "shortName")
+                                     .withIgnoreHeaderCase()
+                                     .withTrim())) {
+
+            for (CSVRecord csvRecord : csvParser) {  //each row has a dictionary
+                HashMap<String, String> data = new HashMap<>();
+                String nodeID = csvRecord.get(1);
+
+                Statement statement =
+                        connection.prepareStatement(
+                                "INSERT INTO Locations(NodeID, xcoord, ycoord, Floor, building, nodeType, longName, shortName)values(?,?,?,?,?,?,?,?)");
+
+                // Accessing values by the names assigned to each column
+
+
+                for (int i = 1; i < 9; i++) { //each item in the row
+                    ((PreparedStatement) statement).setString(i, csvRecord.get(i - 1)); //to access the first value for table it starts at 1
+                    if(i > 1) { //Everything that is not node
+                        data.put(headers[i - 1], csvRecord.get(i - 1));
+                    }
+
+                }
+                Location location = new Location(nodeID, data);
+                locationData.put(location.getNodeID(), location);
+
+
+                ((PreparedStatement) statement).execute();
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
