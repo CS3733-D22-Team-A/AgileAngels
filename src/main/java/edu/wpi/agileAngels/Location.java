ed37fa39e1f9e566b957e5cb42e0520ffd336409
Talia Andrews <tgandrews@wpi.edu>
2022-03-24 17:12:11 -0400

    Update Location.java
    Location updated with nodeID and hashmap. Signatures for getters and setters done.

--- src/main/java/edu/wpi/agileAngels/Location.java
+++ src/main/java/edu/wpi/agileAngels/Location.java
@@ -4,52 +4,35 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.HashMap;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 public class Location {
   // Makes a Location object and reads through CSV FILE and makes table
-  private static final String CSV_FILE_PATH = "./TowerLocations.csv";
+  private String NodeID;
+  private HashMap<String, String> data;
 
-  public void read(Connection connection) throws IOException {
-
-    try (Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
-        CSVParser csvParser =
-            new CSVParser(
-                reader,
-                CSVFormat.DEFAULT
-                    .withHeader(
-                        "NodeID",
-                        "xcoord",
-                        "ycoord",
-                        "floor",
-                        "building",
-                        "nodeType",
-                        "longName",
-                        "shortName")
-                    .withIgnoreHeaderCase()
-                    .withTrim())) {
-
-      for (CSVRecord csvRecord : csvParser) {
-        Statement statement =
-            connection.prepareStatement(
-                "INSERT INTO Locations(NodeID, xcoord, ycoord, Floor, building, nodeType, longName, shortName)values(?,?,?,?,?,?,?,?)");
-
-        // Accessing values by the names assigned to each column
-
-        for (int i = 1; i < 9; i++) {
-          ((PreparedStatement) statement).setString(i, csvRecord.get(i - 1));
-        }
-
-        ((PreparedStatement) statement).execute();
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+  public Location(String nodeID, HashMap<String, String> dataIn){
+    this.NodeID = nodeID;
+    this.data = dataIn;
   }
+
+  public String getNodeID(){}
+  public String getXCoord(){}
+  public String getYCoord(){}
+  public String getFloor(){}
+  public String getNodeType(){}
+  public String getLongName(){}
+  public String getShortName(){}
+  public String setNodeID(){}
+  public String setXCoord(){}
+  public String setYCoord(){}
+  public String setFloor(){}
+  public String setNodeType(){}
+  public String setLongName(){}
+  public String setShortName(){}
 }
