e212664196ce24c90658e29748aec7ee2f5aa12d
hlarrabee <harmonilarrabee@gmail.com>
2022-03-24 15:27:30 -0400

    Fixed choicebox error

--- src/main/java/edu/wpi/agileAngels/Acontroller.java
+++ src/main/java/edu/wpi/agileAngels/Acontroller.java
@@ -1,12 +1,15 @@
 package edu.wpi.agileAngels;
 
 import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class Acontroller {
