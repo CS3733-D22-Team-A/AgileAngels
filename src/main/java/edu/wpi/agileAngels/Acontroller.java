d73124e445a1fbf171a70cb7496988e8694c4abb
jakobsperry <sperryjakob@gmail.com>
2022-03-24 20:53:40 -0400

    added meal request

--- src/main/java/edu/wpi/agileAngels/Acontroller.java
+++ src/main/java/edu/wpi/agileAngels/Acontroller.java
@@ -1,20 +1,27 @@
 package edu.wpi.agileAngels;
 
+import java.awt.*;
 import java.io.IOException;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Acontroller {
 
-  @FXML private Button equipmentButton, labButton, homeButton;
+  @FXML private Button equipmentButton, labButton, homeButton, foodButton;
+  @FXML private MenuButton mealDropdown;
+  @FXML private TextField roomInput;
+  @FXML private TextArea restrictions;
+  @FXML private Label confirm;
+  private String meal = "null";
 
   // Switches to a new scene depending on which button is pressed
   @FXML
@@ -33,15 +40,47 @@ public class Acontroller {
     else if (event.getSource() == labButton) {
       stage = (Stage) equipmentButton.getScene().getWindow();
       root = FXMLLoader.load(getClass().getResource("views/lab-view.fxml"));
+    } else if (event.getSource() == foodButton) {
+      stage = (Stage) foodButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/mealRequest-view.fxml"));
     }
     // If the home button is pressed, switch to the default scene
     else {
       stage = (Stage) homeButton.getScene().getWindow();
       root = FXMLLoader.load(getClass().getResource("views/default-view.fxml"));
     }
-
     Scene scene = new Scene(root);
     stage.setScene(scene);
     stage.show();
+  }
+
+  @FXML
+  private void setChicken() {
+    mealDropdown.setText("Chicken");
+    meal = "Chicken";
+  }
+
+  @FXML
+  private void setSteak() {
+    mealDropdown.setText("steak");
+    meal = "Steak";
+  }
+
+  @FXML
+  private void setSalad() {
+    mealDropdown.setText("Salad");
+    meal = "Salad";
+  }
+
+  @FXML
+  private void placeOrder() {
+    confirm.setText(
+        "Order confirmed to room "
+            + roomInput.getText()
+            + " for "
+            + meal
+            + "\n"
+            + " Special Instructions: "
+            + restrictions.getText());
   }
 }
