e866c5fb265d657cf67f10cfa63aeef7fa6d1ed1
jakobsperry <sperryjakob@gmail.com>
2022-03-24 23:15:11 -0400

    fix lab request

--- src/main/java/edu/wpi/agileAngels/Acontroller.java
+++ src/main/java/edu/wpi/agileAngels/Acontroller.java
@@ -1,7 +1,6 @@
 package edu.wpi.agileAngels;
 
 import java.io.IOException;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,11 +9,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class Acontroller {
@@ -34,9 +33,9 @@ public class Acontroller {
   private String meal = "null";
 
   @FXML private ChoiceBox<String> labCHOICE;
-//Justin's dropdown menu things to be completed.
+  // Justin's dropdown menu things to be completed.
   ObservableList<String> labList =
-          FXCollections.observableArrayList("Blood Test", "Urine Test", "Tumor Marker");
+      FXCollections.observableArrayList("Blood Test", "Urine Test", "Tumor Marker");
 
   // Switches to a new scene depending on which button is pressed
   @FXML
@@ -128,4 +127,7 @@ public class Acontroller {
   private void setMachine() {
     eqptDropdown.setText("Machine");
   }
+
+  @FXML
+  private void getTest() {}
 }
