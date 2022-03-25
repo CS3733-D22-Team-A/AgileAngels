7a3c72551b1a8c7716e455dc61466bc64183ba72
Talia Andrews <tgandrews@wpi.edu>
2022-03-25 09:52:59 -0400

    Added working addLocation and deleteLocation method.

--- src/main/java/edu/wpi/agileAngels/Adb.java
+++ src/main/java/edu/wpi/agileAngels/Adb.java
@@ -2,16 +2,17 @@ package edu.wpi.agileAngels;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
 import java.util.Scanner;
 import javax.swing.*;
 
 public class Adb {
   private HashMap<String, Location> data;
   public Connection connection = null;
+
   public void main(String[] args) throws IOException, InterruptedException {
-    
-    
-    //menu();
+
+    // menu();
     // Apache Derby and table creation
     System.out.println("-------Embedded Apache Derby Connection Testing --------");
     try {
@@ -29,14 +30,13 @@ public class Adb {
     }
 
     System.out.println("Apache Derby driver registered!");
-    Connection connection = null;
 
     Statement statement;
     try {
       // substitute your database name for myDB
       connection = DriverManager.getConnection("jdbc:derby:myDB;create=true");
       statement = connection.createStatement();
-
+      // String query2 = "DROP TABLE Locations";
       String query =
           "CREATE TABLE Locations( "
               + "NodeID VARCHAR(50),"
@@ -47,6 +47,7 @@ public class Adb {
               + "NodeType VARCHAR(50),"
               + "longName VARCHAR(50),"
               + "shortName VARCHAR(50))";
+      // statement.execute(query2);
       statement.execute(query);
 
     } catch (SQLException e) {
@@ -55,12 +56,13 @@ public class Adb {
       return;
     }
     System.out.println("Apache Derby connection established!");
-    Location location = new Location();
-    location.read(connection);
+
     Parser parser = new Parser();
     parser.createTable(connection);
-    data = parser.locationData; //Updates the big hashmap
-
+    data = parser.locationData; // Updates the big hashmap
+    for (int i = 0; i < 1; i++) {
+      menu();
+    }
   }
 
   /** Menu Creation for User* */
@@ -82,34 +84,77 @@ public class Adb {
       System.out.println("Location Information");
       // TODO call location information function
 
-    }
-    if (select.equals("2")) {
+    } else if (select.equals("2")) {
       System.out.println("Change Floor and Type");
       // TODO call change floor and type function
 
-    }
-    if (select.equals("3")) {
-      System.out.println("Enter Location");
-      // TODO call adding location function
-
-    }
-    if (select.equals("4")) {
-      System.out.println("Delete Location");
-      // TODO call delete location function
-
-    }
-    if (select.equals("5")) {
+    } else if (select.equals("3")) {
+      System.out.println("Enter Location ID");
+      Scanner in = new Scanner(System.in);
+      String nodeID = in.next();
+      try {
+        addLocation(nodeID);
+      } catch (SQLException sqlException) {
+        System.out.println("Adding a new location unsuccessful.");
+      }
+      in.close();
+    } else if (select.equals("4")) {
+      System.out.println("Enter Location ID");
+      Scanner in = new Scanner(System.in);
+      String nodeID = in.next();
+      try {
+        deleteLocation(nodeID);
+      } catch (SQLException sqlException) {
+        System.out.println("Location could not be removed.");
+      }
+      in.close();
+    } else if (select.equals("5")) {
       System.out.println("Save Locations to CSV File");
       // TODO call save locations to csv file function
 
-    }
-    if (select.equals("6")) {
+    } else if (select.equals("6")) {
       System.out.println("Exit Program");
       // TODO call exit program function
 
     } else {
       System.out.println("Wrong Input, Select From Menu");
     }
-    menu();
+    myObj.close();
+    // menu();
+  }
+
+  /**
+   * Adds a new location to the location table and hashmap.
+   *
+   * @param node
+   * @throws SQLException
+   */
+  private void addLocation(String node) throws SQLException {
+    // Adding to the database table
+    String add =
+        "INSERT INTO Locations(NodeID,xcoord,ycoord,Floor,building,nodeType,longName,shortName)VALUES(?,'?','?','?','?','?','?','?')";
+    PreparedStatement preparedStatement = connection.prepareStatement(add);
+    preparedStatement.setString(1, node);
+    preparedStatement.execute();
+
+    // Adding to the hashmap
+    data.put(node, new Location(node, new HashMap<>()));
+  }
+
+  /**
+   * Deletes a location from the table and hashmap.
+   *
+   * @param node
+   * @throws SQLException
+   */
+  private void deleteLocation(String node) throws SQLException {
+    // Deleting from the database table
+    PreparedStatement preparedStatement =
+        connection.prepareStatement("DELETE FROM Locations WHERE NodeID = ?");
+    preparedStatement.setString(1, node);
+    preparedStatement.execute();
+
+    // Deleting from hashmap
+    data.remove(node);
   }
 }
