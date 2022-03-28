78e20fedfbd4a8ddb6e18684e511c4e086170084
physicalfrog4 <77911997+physicalfrog4@users.noreply.github.com>
2022-03-28 12:35:48 -0400

    Rename file because I can't spell

--- src/main/java/edu/wpi/agileAngels/LocationDAO.java
+++ src/main/java/edu/wpi/agileAngels/LocationDAO.java
@@ -1,17 +1,24 @@
 package edu.wpi.agileAngels;
-import java.util.List;
 
 //includes the four DOA methods
-public interface LocationDOA
-{
+public interface LocationDAO {
+
     pubilc List<Location> getAllLocations(); // error because of Location hides fields
+
     public Location getLocation(String NodeID);
+
     public void updateLocationType(Location location);
+
     public void updateLocationFloor(Location location);
+
     public void updateLocationLongName(Location location);
+
     public void updateLocationShortName(Location location);
+
     public void updateLocationXCoord(Location location);
+
     public void updateLocationYCoord(Location location);
+
     public void deleteLocation(Location location);
 
 }
--- src/main/java/edu/wpi/agileAngels/LocationDAOImpl.java
+++ src/main/java/edu/wpi/agileAngels/LocationDAOImpl.java
@@ -3,54 +3,64 @@ package edu.wpi.agileAngels;
 import java.util.ArrayList;
 import java.util.List;
 
-//includes a list od Location objects and implements the Location DOA methods
-public class LocationDOAImpl implements LocationDAO {
+//includes a list od Location objects and implements the Location DAO methods
+//this will get the data from the DB?
+public class LocationDAOImpl implements LocationDAO {
     //List is working as a database
     List<Location> locations;
 
     public LocationDAOImpl() { //error, maybe return void? doesn't in tutorial :(
         locations = new ArrayList<Location>();
-        Location loc1 = new Location(); //add values
-        Location loc2 = new Location(); //add values
+        Location loc1 = new Location(); //add values (figure out if we need this)
+        Location loc2 = new Location(); //add values (figure out if we need this)
     }
 
     //@Override
     // override is in the tutorial, maybe change method name to delete?
+    //use the dictionaries here instead of this method (idea)
     public void deleteLocation(Location location) {
         locations.remove(location.getNodeID());
         System.out.println("Location: NodeID " + location.getNodeID() + ", deleted from the database");
     }
+
     //retrieve list of locations from the database
-   @Overrride
-   public List<Location> getAllLocations(){
-     return Locations;
+    @Override
+    public List<Location> getAllLocations() {
+        return Locations;
     }
+
     // @Override override in the tutorial, different method name?
-    public Location getLocation(String NodeID){
-    return Locations.get(NodeID);
+    public Location getLocation(String NodeID) {
+        return Locations.get(NodeID);
     }
+
     // @Override override in the tutoral, different method name? (for all updateLocation<field_name> methods)
-    public void updateLocationType(Location location){
+    public void updateLocationType(Location location) {
         locations.get(location.getNodeID()).setNodeType(location.getNodeType()); //error here with getter
         System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
-    }ad
-    public void updateLocationFloor(Location location){
+    }
+
+    public void updateLocationFloor(Location location) {
         locations.get(location.getNodeID()).setFloor(location.getFloor()); //error here with getter
         System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
     }
-    public void updateLocationLongName(Location location){
+
+    public void updateLocationLongName(Location location) {
         locations.get(location.getNodeID()).setLongName(location.getLongName()); //error here with getter
         System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
     }
-    public void updateLocationShortName(Location location){
+
+    public void updateLocationShortName(Location location) {
         locations.get(location.getNodeID()).setShortName(location.getShortName()); //error here with getter
         System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
     }
-    public void updateLocationXCoord(Location location){
+
+    public void updateLocationXCoord(Location location) {
         locations.get(location.getNodeID()).setXCoord(location.getXCoord()); //error here with getter
         System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
     }
-    public void updateLocationYCoord(Location location){
+
+    public void updateLocationYCoord(Location location) {
         locations.get(location.getNodeID()).setYCoord(location.getYCoord()); //error here with getter
         System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
     }
