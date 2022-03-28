36833b6f066778e832a696d2b0d8ced6bcb8717c
physicalfrog4 <77911997+physicalfrog4@users.noreply.github.com>
2022-03-28 10:21:09 -0400

    adding methods and List

--- src/main/java/edu/wpi/agileAngels/LocationDOA.java
+++ src/main/java/edu/wpi/agileAngels/LocationDOA.java
@@ -1,5 +1,17 @@
 package edu.wpi.agileAngels;
+import java.util.List;
+
 //includes the four DOA methods
 public interface LocationDOA
 {
+    pubilc List<Location> getAllLocations(); // error because of Location hides fields
+    public Location getLocation(String NodeID);
+    public void updateLocationType(Location location);
+    public void updateLocationFloor(Location location);
+    public void updateLocationLongName(Location location);
+    public void updateLocationShortName(Location location);
+    public void updateLocationXCoord(Location location);
+    public void updateLocationYCoord(Location location);
+    public void deleteLocation(Location location);
+
 }
