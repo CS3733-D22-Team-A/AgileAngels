68f85d8755a773d63caf7d901afb94608a265f88
physicalfrog4 <77911997+physicalfrog4@users.noreply.github.com>
2022-03-28 09:42:56 -0400

    added new Classes for the backend

---
+++ src/main/java/edu/wpi/agileAngels/LocationDOA.java
@@ -0,0 +1,5 @@
+package edu.wpi.agileAngels;
+//includes the four DOA methods
+public interface LocationDOA
+{
+}
---
+++ src/main/java/edu/wpi/agileAngels/LocationDOAImpl.java
@@ -0,0 +1,4 @@
+package edu.wpi.agileAngels;
+//includes a list od Location objects and implements the Location DOA methods
+public class LocationDOAImpl {
+}
