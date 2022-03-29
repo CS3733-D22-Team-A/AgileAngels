b913cf9c3f6c1c2eb2f8ba65e09eafa251e3fa9d
jakobsperry <sperryjakob@gmail.com>
2022-03-29 15:53:45 -0400

    constructor written at record speeds

--- src/main/java/edu/wpi/agileAngels/Request.java
+++ src/main/java/edu/wpi/agileAngels/Request.java
@@ -1,4 +1,15 @@
 package edu.wpi.agileAngels;
 
 public class Request {
+    String employeeName;
+    String location;
+    String type;
+
+    public Request(String employeeName, String location, String type) {
+        this.employeeName = employeeName;
+        this.location = location;
+        this.type = type;
+    }
+
+
 }
