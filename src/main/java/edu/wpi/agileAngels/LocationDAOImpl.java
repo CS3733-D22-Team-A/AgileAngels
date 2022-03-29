b0f03f71eeeab38e0fbda3dc24fab8f3767ada3b
physicalfrog4 <77911997+physicalfrog4@users.noreply.github.com>
2022-03-29 10:27:46 -0400

    updatefunctions rewritten

--- src/main/java/edu/wpi/agileAngels/LocationDAOImpl.java
+++ src/main/java/edu/wpi/agileAngels/LocationDAOImpl.java
@@ -1,67 +1,127 @@
 package edu.wpi.agileAngels;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
-//includes a list od Location objects and implements the Location DAO methods
-//this will get the data from the DB?
+// includes a list od Location objects and implements the Location DAO methods
+// this will get the data from the DB?
 public class LocationDAOImpl implements LocationDAO {
-    //List is working as a database
-    List<Location> locations;
+    // List is working as a database
+    // List<Location> locations;
+    HashMap<String, Location> locations = new HashMap<>();
 
-    public LocationDAOImpl() { //error, maybe return void? doesn't in tutorial :(
-        locations = new ArrayList<Location>();
-        Location loc1 = new Location(); //add values (figure out if we need this)
-        Location loc2 = new Location(); //add values (figure out if we need this)
-    }
+    public LocationDAOImpl() { // error, maybe return void? doesn't in tutorial :(
+        //  locations = new ArrayList<Location>();
+       }
 
-    //@Override
+    // @Override
     // override is in the tutorial, maybe change method name to delete?
-    //use the dictionaries here instead of this method (idea)
+    // use the dictionaries here instead of this method (idea)
     public void deleteLocation(Location location) {
         locations.remove(location.getNodeID());
         System.out.println("Location: NodeID " + location.getNodeID() + ", deleted from the database");
     }
 
-    //retrieve list of locations from the database
-    @Override
-    public List<Location> getAllLocations() {
-        return Locations;
+    // retrieve list of locations from the database
+
+    public HashMap<String, String> getAllLocations() {
+        // return locations;
+        return null; // error we need to return a hashmap of locations
     }
 
     // @Override override in the tutorial, different method name?
     public Location getLocation(String NodeID) {
-        return Locations.get(NodeID);
+        return locations.get(NodeID);
     }
 
-    // @Override override in the tutoral, different method name? (for all updateLocation<field_name> methods)
-    public void updateLocationType(Location location) {
-        locations.get(location.getNodeID()).setNodeType(location.getNodeType()); //error here with getter
-        System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
-    }
-
-    public void updateLocationFloor(Location location) {
-        locations.get(location.getNodeID()).setFloor(location.getFloor()); //error here with getter
-        System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
-    }
-
-    public void updateLocationLongName(Location location) {
-        locations.get(location.getNodeID()).setLongName(location.getLongName()); //error here with getter
-        System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
-    }
-
-    public void updateLocationShortName(Location location) {
-        locations.get(location.getNodeID()).setShortName(location.getShortName()); //error here with getter
-        System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
-    }
+    // loc1 will have the one value that we want to take and replace in the other location
+    // loc2 will have the location with the other values we DON'T want to replace
 
     public void updateLocationXCoord(Location location) {
-        locations.get(location.getNodeID()).setXCoord(location.getXCoord()); //error here with getter
-        System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
+        String NodeID = location.getNodeID();
+        Location loc1 = location;
+        Location loc2 = locations.get(NodeID);
+        Location locNew =
+                new Location(loc2.getNodeID(), loc1.getXCoord(), loc2.getYCoord(),
+                        loc2.getFloor(), loc2.getBuilding(), loc2.getNodeType(),
+                        loc2.getLongName(), loc2.getShortName());
+        locations.replace(NodeID, locNew);
+        System.out.println("Location: NodeID " + NodeID + ", updated in the database");
     }
 
     public void updateLocationYCoord(Location location) {
-        locations.get(location.getNodeID()).setYCoord(location.getYCoord()); //error here with getter
-        System.out.println("Location: NodeID " + location.getNodeID() + ", updated in the database");
+        String NodeID = location.getNodeID();
+        Location loc1 = location;
+        Location loc2 = locations.get(NodeID);
+        Location locNew =
+                new Location(loc2.getNodeID(), loc2.getXCoord(), loc1.getYCoord(),
+                        loc2.getFloor(), loc2.getBuilding(), loc2.getNodeType(),
+                        loc2.getLongName(), loc2.getShortName());
+        locations.replace(NodeID, locNew);
+        System.out.println("Location: NodeID " + NodeID + ", updated in the database");
     }
+
+    public void updateLocationFloor(Location location) {
+        String NodeID = location.getNodeID();
+        Location loc1 = location;
+        Location loc2 = locations.get(NodeID);
+        Location locNew =
+                new Location(loc2.getNodeID(), loc2.getXCoord(), loc2.getYCoord(),
+                        loc1.getFloor(), loc2.getBuilding(), loc2.getNodeType(),
+                        loc2.getLongName(), loc2.getShortName());
+        locations.replace(NodeID, locNew);
+        System.out.println("Location: NodeID " + NodeID + ", updated in the database");
+    }
+
+    public void updateLocationBuilding(Location location) {
+        String NodeID = location.getNodeID();
+        Location loc1 = location;
+        Location loc2 = locations.get(NodeID);
+        Location locNew =
+                new Location(loc2.getNodeID(), loc2.getXCoord(), loc2.getYCoord(),
+                        loc2.getFloor(), loc1.getBuilding(), loc2.getNodeType(),
+                        loc2.getLongName(), loc2.getShortName());
+        locations.replace(NodeID, locNew);
+        System.out.println("Location: NodeID " + NodeID + ", updated in the database");
+    }
+
+
+    // @Override override in the tutoral, different method name? (for all updateLocation<field_name>
+    // methods)
+    public void updateLocationType(Location location) {
+        String NodeID = location.getNodeID();
+        Location loc1 = location;
+        Location loc2 = locations.get(NodeID);
+        Location locNew =
+                new Location(loc2.getNodeID(), loc2.getXCoord(), loc2.getYCoord(),
+                        loc2.getFloor(), loc2.getBuilding(), loc1.getNodeType(),
+                        loc2.getLongName(), loc2.getShortName());
+        locations.replace(NodeID, locNew);
+        System.out.println("Location: NodeID " + NodeID + ", updated in the database");
+    }
+
+
+    public void updateLocationLongName(Location location) {
+        String NodeID = location.getNodeID();
+        Location loc1 = location;
+        Location loc2 = locations.get(NodeID);
+        Location locNew =
+                new Location(loc2.getNodeID(), loc2.getXCoord(), loc2.getYCoord(),
+                        loc2.getFloor(), loc2.getBuilding(), loc2.getNodeType(),
+                        loc1.getLongName(), loc2.getShortName());
+        locations.replace(NodeID, locNew);
+        System.out.println("Location: NodeID " + NodeID + ", updated in the database");
+    }
+
+    public void updateLocationShortName(Location location) {
+        String NodeID = location.getNodeID();
+        Location loc1 = location;
+        Location loc2 = locations.get(NodeID);
+        Location locNew =
+                new Location(loc2.getNodeID(), loc2.getXCoord(), loc2.getYCoord(),
+                        loc2.getFloor(), loc2.getBuilding(), loc2.getNodeType(),
+                        loc2.getLongName(), loc1.getShortName());
+        locations.replace(NodeID, locNew);
+        System.out.println("Location: NodeID " + NodeID + ", updated in the database");
+    }
+
 }
