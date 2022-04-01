e033ce0b700fe726103f98f627dafd00ae6d670a
jakobsperry <sperryjakob@gmail.com>
2022-04-01 00:30:32 -0400

    more UI things, i have some base stuff down and buttons working the way i want. I also have a empty page file done to start off of so they are all the same

--- src/main/java/edu/wpi/agileAngels/MainController.java
+++ src/main/java/edu/wpi/agileAngels/MainController.java
@@ -55,105 +55,105 @@ public class MainController {
     }
   }
 
-  //  @FXML
-  //  private void openScene(ActionEvent event) throws IOException {
-  //    Stage stage;
-  //    Parent root;
-  //    // If the equipment request button on the default scene is pressed,
-  //    // switch to the equipment scene
-  //    if (event.getSource() == equipmentButton) {
-  //      stage = (Stage) equipmentButton.getScene().getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/equipment-view.fxml"));
-  //    } else if (event.getSource() == locationButton) {
-  //      stage = (Stage) locationButton.getScene().getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/locations-view.fxml"));
-  //    } else if (event.getSource() == equipmentMenu) {
-  //      stage =
-  //          (Stage)
-  //              ((MenuItem) event.getTarget())
-  //                  .getParentPopup()
-  //                  .getOwnerWindow()
-  //                  .getScene()
-  //                  .getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/equipment-view.fxml"));
-  //    }
-  //    // If the lab request button on the default scene is pressed,
-  //    // switch to the lab scene
-  //    else if (event.getSource() == labMenu) {
-  //      stage =
-  //          (Stage)
-  //              ((MenuItem) event.getTarget())
-  //                  .getParentPopup()
-  //                  .getOwnerWindow()
-  //                  .getScene()
-  //                  .getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/lab-view.fxml"));
-  //    } else if (event.getSource() == labButton) {
-  //      stage = (Stage) labButton.getScene().getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/lab-view.fxml"));
-  //    }
-  //    // If the meal request button on the default scene is pressed,
-  //    // switch to the meal scene
-  //    else if (event.getSource() == mealMenu) {
-  //      stage =
-  //          (Stage)
-  //              ((MenuItem) event.getTarget())
-  //                  .getParentPopup()
-  //                  .getOwnerWindow()
-  //                  .getScene()
-  //                  .getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/mealRequest-view.fxml"));
-  //    } else if (event.getSource() == foodButton) {
-  //      stage = (Stage) foodButton.getScene().getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/mealRequest-view.fxml"));
-  //    }
-  //
-  //    // If the sanitation request button on the default scene is pressed,
-  //    // switch to the sanitation scene
-  //    else if (event.getSource() == sanitationMenu) {
-  //      stage =
-  //          (Stage)
-  //              ((MenuItem) event.getTarget())
-  //                  .getParentPopup()
-  //                  .getOwnerWindow()
-  //                  .getScene()
-  //                  .getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/sanitation-view.fxml"));
-  //    } else if (event.getSource() == sanitationButton) {
-  //      stage = (Stage) sanitationButton.getScene().getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/sanitation-view.fxml"));
-  //    }
-  //    // If the gift request button on the default scene is pressed,
-  //    // switch to the gift scene
-  //    else if (event.getSource() == giftMenu) {
-  //      stage =
-  //          (Stage)
-  //              ((MenuItem) event.getTarget())
-  //                  .getParentPopup()
-  //                  .getOwnerWindow()
-  //                  .getScene()
-  //                  .getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/gifts-view.fxml"));
-  //    } else if (event.getSource() == giftButton) {
-  //      stage = (Stage) giftButton.getScene().getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/gifts-view.fxml"));
-  //    } else if (event.getSource() == mapButton) {
-  //      stage = (Stage) mapButton.getScene().getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/maps.fxml"));
-  //    }
-  //    // If the home button is pressed, switch to the default scene
-  //    else {
-  //      stage = (Stage) homeButton.getScene().getWindow();
-  //      root = FXMLLoader.load(getClass().getResource("views/default-view.fxml"));
-  //    }
-  //
-  //    Scene scene = new Scene(root);
-  //    stage.setScene(scene);
-  //    stage.show();
-  //  }
+  @FXML
+  private void openScene(ActionEvent event) throws IOException {
+    Stage stage;
+    Parent root;
+    // If the equipment request button on the default scene is pressed,
+    // switch to the equipment scene
+    if (event.getSource() == equipmentButton) {
+      stage = (Stage) equipmentButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/equipment-view.fxml"));
+    } else if (event.getSource() == locationButton) {
+      stage = (Stage) locationButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/locations-view.fxml"));
+    } else if (event.getSource() == equipmentMenu) {
+      stage =
+          (Stage)
+              ((MenuItem) event.getTarget())
+                  .getParentPopup()
+                  .getOwnerWindow()
+                  .getScene()
+                  .getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/equipment-view.fxml"));
+    }
+    // If the lab request button on the default scene is pressed,
+    // switch to the lab scene
+    else if (event.getSource() == labMenu) {
+      stage =
+          (Stage)
+              ((MenuItem) event.getTarget())
+                  .getParentPopup()
+                  .getOwnerWindow()
+                  .getScene()
+                  .getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/lab-view.fxml"));
+    } else if (event.getSource() == labButton) {
+      stage = (Stage) labButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/lab-view.fxml"));
+    }
+    // If the meal request button on the default scene is pressed,
+    // switch to the meal scene
+    else if (event.getSource() == mealMenu) {
+      stage =
+          (Stage)
+              ((MenuItem) event.getTarget())
+                  .getParentPopup()
+                  .getOwnerWindow()
+                  .getScene()
+                  .getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/mealRequest-view.fxml"));
+    } else if (event.getSource() == foodButton) {
+      stage = (Stage) foodButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/mealRequest-view.fxml"));
+    }
+
+    // If the sanitation request button on the default scene is pressed,
+    // switch to the sanitation scene
+    else if (event.getSource() == sanitationMenu) {
+      stage =
+          (Stage)
+              ((MenuItem) event.getTarget())
+                  .getParentPopup()
+                  .getOwnerWindow()
+                  .getScene()
+                  .getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/sanitation-view.fxml"));
+    } else if (event.getSource() == sanitationButton) {
+      stage = (Stage) sanitationButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/sanitation-view.fxml"));
+    }
+    // If the gift request button on the default scene is pressed,
+    // switch to the gift scene
+    else if (event.getSource() == giftMenu) {
+      stage =
+          (Stage)
+              ((MenuItem) event.getTarget())
+                  .getParentPopup()
+                  .getOwnerWindow()
+                  .getScene()
+                  .getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/gifts-view.fxml"));
+    } else if (event.getSource() == giftButton) {
+      stage = (Stage) giftButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/gifts-view.fxml"));
+    } else if (event.getSource() == mapButton) {
+      stage = (Stage) mapButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/maps.fxml"));
+    }
+    // If the home button is pressed, switch to the default scene
+    else {
+      stage = (Stage) homeButton.getScene().getWindow();
+      root = FXMLLoader.load(getClass().getResource("views/default-view.fxml"));
+    }
+
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
 
   @FXML
-  private void goHome() throws IOException {
+  private void goHome(ActionEvent event) throws IOException {
     loadPage("views/home-view.fxml", close);
   }
 
