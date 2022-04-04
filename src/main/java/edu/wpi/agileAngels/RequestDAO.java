b7457474e93291b6bdee675c9240e38c7f72a9b6
physicalfrog4 <77911997+physicalfrog4@users.noreply.github.com>
2022-04-04 12:53:54 -0400

    add two classes for request impl

---
+++ src/main/java/edu/wpi/agileAngels/RequestDAO.java
@@ -0,0 +1,24 @@
+package edu.wpi.agileAngels;
+
+import java.util.HashMap;
+
+public interface RequestDAO {
+
+  public HashMap<String, Request> getAllMedicalEquipmentRequests();
+
+  public void updateName(MedDevice medDevice, String newName);
+
+  public void updateAvailability(MedDevice medDevice, String newAvailability);
+
+  public void updateType(MedDevice medDevice, String newType);
+
+  public void updateLocation(MedDevice medDevice, String newLocation);
+
+  public void updateEmployee(MedDevice medDevice, String newEmployee);
+
+  public void updateStatus(MedDevice medDevice, String newStatus);
+
+  public void deleteMed(MedDevice medDevice);
+
+  public void addMed(MedDevice medDevice);
+}
---
+++ src/main/java/edu/wpi/agileAngels/RequestDAOImpl.java
@@ -0,0 +1,3 @@
+package edu.wpi.agileAngels;
+
+public class RequestDAOImpl {}
