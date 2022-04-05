15d1c917a00781d95023ceac5837ae807b53ed74
aadhyaputtur <putturaadhya@gmail.com>
2022-04-04 20:10:28 -0400

    Adding a counter for the id of the request to the request DAO impl

--- src/main/java/edu/wpi/agileAngels/RequestDAOImpl.java
+++ src/main/java/edu/wpi/agileAngels/RequestDAOImpl.java
@@ -2,10 +2,17 @@ package edu.wpi.agileAngels;
 
 import java.util.HashMap;
 
-public class RequestDAOImpl implements RequestDAO {
+public class RequestDAOImpl {
 
-  private final String CSV_FILE_PATH = "./MedData.csv";
+  private String CSV_FILE_PATH = "./MedData.csv";
   private HashMap<String, Request> reqData = new HashMap<>();
+  private int count;
+
+  public RequestDAOImpl(String CSV_FILE_PATH, HashMap<String, Request> reqData, int count){
+      this.CSV_FILE_PATH = CSV_FILE_PATH;
+      this.reqData = reqData;
+      this.count = count;
+  }
 
   public HashMap<String, Request> getAllRequests() {
     return reqData;
@@ -16,30 +23,32 @@ public class RequestDAOImpl implements RequestDAO {
   }
 
   public void updateRequestType(Request request, int requestType) {
-    request.setRequestType(requestType);
+      request.setRequestType(requestType);
   }
 
   public void updateType(Request request, String newType) {
-    request.setType(newType);
+      request.setType(newType);
   }
 
   public void updateLocation(Request request, String newLocation) {
-    request.setLocation(newLocation);
+      request.setLocation(newLocation);
   }
 
   public void updateDescription(Request request, String description) {
-    request.setDescription(description);
+      request.setDescription(description);
   }
 
   public void updateStatus(Request request, String newStatus) {
-    request.setStatus(newStatus);
+      request.setStatus(newStatus);
   }
 
   public void deleteMed(Request request) {
-    reqData.remove(request.getDescription()); // change to the key
+      reqData.remove(request.getDescription()); //change to the key
   }
 
   public void addMed(Request request) {
-    reqData.put(request.getDescription(), request); // change to the key
+      reqData.put(request.getDescription(), request);
+      count = count +1;
+      //change to the key
   }
 }
