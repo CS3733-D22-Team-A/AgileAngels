99f1e1edb3eda82c2d212e19af1108b1507e947b
Talia Andrews <tgandrews@wpi.edu>
2022-04-05 20:59:01 -0400

    MedicalEquip. Pair programmed with Aaron.

---
+++ src/main/java/edu/wpi/agileAngels/MedicalEquip.java
@@ -0,0 +1,12 @@
+package edu.wpi.agileAngels;
+
+public class MedicalEquip {
+    private String name;
+    private int amount;
+
+    public MedicalEquip(String name, int amount){
+
+        this.name = name;
+        this.amount = amount;
+    }
+}
