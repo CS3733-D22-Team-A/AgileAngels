8a5378fc57d960c59b2cd476c6f056ea83916052
InDanity <101886886+InDanity@users.noreply.github.com>
2022-04-07 20:53:19 -0400

    New Employee class. :D

---
+++ src/main/java/edu/wpi/agileAngels/Employee.java
@@ -0,0 +1,51 @@
+package edu.wpi.agileAngels;
+
+import java.util.ArrayList;
+
+public class Employee {
+    private String name;
+    private String username;
+    private String password;
+    private ArrayList<Request> requests;
+
+    public Employee(String name, String username, String password, ArrayList<Request> requests){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.requests = requests;
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String  getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String pass){
+        this.password = pass;
+    }
+
+    public ArrayList<Request> getRequests(ArrayList<Request> requests){
+        return this.requests;
+    }
+
+    public void addRequest(Request newReq){
+        this.requests.add(newReq);
+    }
+
+    public void removeRequest(Request oldReq){
+        this.requests.remove(oldReq);
+    }
+
+}
