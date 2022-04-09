e5e5e761e0de7229b6a74d9b05fdc34616f2dbaa
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2022-04-08 21:44:11 -0400

    Employee Table implementation
    Lets hope it works with the Employee class

--- src/main/java/edu/wpi/agileAngels/Database/EmployeeTable.java
+++ src/main/java/edu/wpi/agileAngels/Database/EmployeeTable.java
@@ -1,31 +1,86 @@
 package edu.wpi.agileAngels.Database;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class EmployeeTable implements TableI {
 
     @Override
     public boolean add(Object obj) {
-        return false;
+        try {
+            if(!(obj instanceof Employees)){
+                return false;
+            }
+            Employee emp = (Employees) obj;
+            String add =
+                    "INSERT INTO Employees(name, requests)VALUES(?,?)";
+            PreparedStatement preparedStatement = DBconnection.getConnection().prepareStatement(add);
+            preparedStatement.setString(1, emp.getName());
+            preparedStatement.setString(2, emp.getRequests());
+            preparedStatement.execute();
+            return true;
+        }catch(SQLException sqlException){
+            return false;
+        }
     }
 
     @Override
-    public boolean delete(String str) {
-        return false;
-    }
+    public boolean delete(String str){
+            try {
+                PreparedStatement preparedStatement =
+                        DBconnection.getConnection().prepareStatement("DELETE FROM Employees WHERE name = ?");
+                preparedStatement.setString(1, str);
+                preparedStatement.execute();
+                return true;
+            } catch (SQLException sqlException) {
+                return false;
+            }
+        }
 
-    @Override
-    public boolean update(Object obj) {
-        return false;
-    }
+            @Override
+            public boolean update (Object obj){
+            try {
+                if (!(obj instanceof Employee)) {
+                    return false;
+                }
+                Employee emp = (Employees) obj;
+                PreparedStatement preparedStatement =
+                        DBconnection.getConnection().prepareStatement("UPDATE RequestTable SET requests = ? WHERE name = ?");
+                preparedStatement.setString(1, emp.getRequests());
+                preparedStatement.setString(2, emp.getName());
+                preparedStatement.execute();
+                return true;
+            } catch (SQLException sqlException) {
+                return false;
+            }
+        }
 
-    @Override
-    public boolean createTable() {
-        return false;
-    }
+            @Override
+            public boolean createTable () {
+            try {
+                Statement query = DBconnection.getConnection().createStatement();
+                String queryEmployees = "CREATE TABLE Employees( "
+                        + "name VARCHAR(50),"
+                        + "requests VARCHAR(50))";
+                query.execute(queryEmployees);
+                return true;
+                } catch (SQLException sqlException) {
+                return false;
+                }
+            }
 
-    @Override
-    public boolean dropTable() {
-        return false;
+
+            @Override
+            public boolean dropTable () {
+            try {
+                Statement droptable = DBconnection.getConnection().createStatement();
+                String dropLoc = "DROP TABLE Employees";
+                droptable.execute(dropLoc);
+                return true;
+                } catch (SQLException sqlException) {
+                return false;
+                }
+            }
     }
-}
