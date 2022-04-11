c183e3c1baeafa3e6c947d0548929e6edca9b192
Justin <73486390+Sombero@users.noreply.github.com>
2022-04-11 16:38:56 -0400

    Fixing equipment request weird error.

--- src/main/java/edu/wpi/agileAngels/Controllers/ServiceRequestController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/ServiceRequestController.java
@@ -18,12 +18,12 @@ public class ServiceRequestController extends MainController implements Initiali
   @FXML Button equipmentRequest, labRequest, sanRequest, mealRequest, giftRequest, testButton;
   @FXML private JFXToggleButton toggleButton;
 
-  // These are/will be the hidden labels for the toggable switch.
+  // These are/will be the hidden labels for the toggleable switch.
   @FXML private Label harmoniLabel, jakobLabel, justinLabel, danielLabel, basharLabel, jakobLabel2;
 
   /**
    * showCreators() will make invisible labels displaying people's names visible again. And turn
-   * them off. "I don't know why, I don't want to know why" - TF2 source code. btw don't touch this
+   * them off. "I don't know why, I don't want to know why" - TF2 source code. btw don't touch this,
    * or I'll throw hands. ~<3
    */
   public void showCreators(ActionEvent event) {
@@ -32,15 +32,20 @@ public class ServiceRequestController extends MainController implements Initiali
             Arrays.asList(
                 harmoniLabel, jakobLabel, justinLabel, danielLabel, basharLabel, jakobLabel2));
     if (toggleButton.isSelected()) {
-      // Will turn on names.
+      // Will turn off names.
       creatorLabels.forEach((coder) -> coder.setVisible(true));
-
     } else {
-      // Will turn off the names.
+      // Will turn on the names.
       creatorLabels.forEach((coder) -> coder.setVisible(false));
     }
   }
 
+  /**
+   * This will bring you to the various request pages when a button is clicked.
+   *
+   * @param event
+   * @throws IOException
+   */
   public void requestButton(ActionEvent event) throws IOException {
     if (event.getSource() == equipmentRequest) {
       loadPage("../views/equipment-view.fxml", equipmentRequest);
