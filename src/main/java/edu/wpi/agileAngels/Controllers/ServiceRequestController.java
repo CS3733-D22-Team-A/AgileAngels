a003f39392fac7d2db48b002051c98938258aed8
Justin <73486390+Sombero@users.noreply.github.com>
2022-04-10 22:57:05 -0400

    Did a f--kton. Made the new Services page and found a way to align the texts toggles/labels correctly. Commented out some code that caused errors that we didn't squash for some reason.

--- src/main/java/edu/wpi/agileAngels/Controllers/ServiceRequestController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/ServiceRequestController.java
@@ -1,14 +1,45 @@
 package edu.wpi.agileAngels.Controllers;
 
+import com.jfoenix.controls.JFXToggleButton;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 // brings you to pages
-public class ServiceRequestController extends MainController {
+public class ServiceRequestController extends MainController implements Initializable {
 
   @FXML Button equipmentRequest, labRequest, sanRequest, mealRequest, giftRequest, testButton;
+  @FXML private JFXToggleButton toggleButton;
+
+  // These are/will be the hidden labels for the toggable switch.
+  @FXML private Label harmoniLabel, jakobLabel, justinLabel, danielLabel, basharLabel, jakobLabel2;
+
+  /**
+   * showCreators() will make invisible labels displaying people's names visible again. And turn
+   * them off. "I don't know why, I don't want to know why" - TF2 source code. btw don't touch this
+   * or I'll throw hands. ~<3
+   */
+  public void showCreators(ActionEvent event) {
+    ArrayList<Label> creatorLabels =
+        new ArrayList<>(
+            Arrays.asList(
+                harmoniLabel, jakobLabel, justinLabel, danielLabel, basharLabel, jakobLabel2));
+    if (toggleButton.isSelected()) {
+      // Will turn on names.
+      creatorLabels.forEach((coder) -> coder.setVisible(true));
+
+    } else {
+      // Will turn off the names.
+      creatorLabels.forEach((coder) -> coder.setVisible(false));
+    }
+  }
 
   public void requestButton(ActionEvent event) throws IOException {
     if (event.getSource() == equipmentRequest) {
@@ -30,4 +61,7 @@ public class ServiceRequestController extends MainController {
       loadPage("../views/test-view.fxml", equipmentRequest);
     }
   }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
 }
