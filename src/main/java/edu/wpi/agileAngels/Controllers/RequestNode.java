a7d8e39793aed9ec0fc963577014ad5556561bef
hlarrabee <harmonilarrabee@gmail.com>
2022-04-11 17:38:52 -0400

    deleted unnecessary code

--- src/main/java/edu/wpi/agileAngels/Controllers/RequestNode.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/RequestNode.java
@@ -7,20 +7,18 @@ import edu.wpi.agileAngels.Database.Request;
 import java.util.HashMap;
 import javafx.event.ActionEvent;
 
-public class RequestNode extends Node {
+public class RequestNode {
 
   private Request request;
   private Location location;
   private RequestNodeManager requestNodeManager;
   private JFXButton button = new JFXButton();
   private LocationDAOImpl locationDAO = new LocationDAOImpl();
-  HashMap<String, Location> locationsHash = new HashMap<>();
+  HashMap<String, Location> locationsHash = locationDAO.getAllLocations();
 
   public RequestNode(Request request, RequestNodeManager requestNodeManager) {
     this.request = request;
     this.requestNodeManager = requestNodeManager;
-
-    this.locationsHash = locationDAO.getAllLocations();
     this.location = locationsHash.get(this.request.getLocation());
 
     button.setLayoutX((this.location.getXCoord() - 800) / 5);
@@ -32,18 +30,6 @@ public class RequestNode extends Node {
         });
 
     button.setStyle("-fx-background-color: #6a78fc; ");
-
-    // set the circle color to coordinate with the node type
-    // if(this.getNodeType() == "Test") {
-    // button.setId("blue"); // this ID will be used in the CSS file to style the button
-    // }
-
-  }
-
-  public void resetLocation() {
-    button.setLayoutX((this.location.getXCoord() - 800) / 5);
-    button.setLayoutY((this.location.getYCoord() - 350) / 5);
-    button.setText(String.valueOf(request.getRequestType()));
   }
 
   public void isClicked() {
@@ -74,24 +60,7 @@ public class RequestNode extends Node {
     return request.getStatus();
   }
 
-  public int getRequestType() {
-    return request.getRequestType();
-  }
-
   public JFXButton getButton() {
     return button;
-  }
-
-  public void changeLocation(String locationID) {
-    this.request.setLocation(locationID);
-    this.location = locationsHash.get(locationID);
-  }
-
-  public void changeEmployee(String employee) {
-    this.request.setEmployee(employee);
-  }
-
-  public void changeStatus(String status) {
-    this.request.setStatus(status);
   }
 }
--- src/main/java/edu/wpi/agileAngels/Controllers/RequestNodeManager.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/RequestNodeManager.java
@@ -1,50 +1,19 @@
 package edu.wpi.agileAngels.Controllers;
 
-import edu.wpi.agileAngels.Database.Location;
-import edu.wpi.agileAngels.Database.LocationDAOImpl;
 import edu.wpi.agileAngels.Database.Request;
 import edu.wpi.agileAngels.Database.RequestDAOImpl;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class RequestNodeManager extends NodeManager {
+public class RequestNodeManager {
 
   private MapsController mapsController;
   private RequestDAOImpl requestDAO = RequestDAOImpl.getInstance("MedRequest");
   private HashMap<String, RequestNode> nodes = new HashMap<>();
-  private int[][] typeCounts = new int[5][5];
-  private HashMap<String, Integer> floorsAndTypes = new HashMap<>();
-  private LocationDAOImpl locationDAO = new LocationDAOImpl();
-  private HashMap<String, Location> locationsHash = new HashMap<>();
 
   public RequestNodeManager(MapsController mapsController) throws SQLException {
     this.mapsController = mapsController;
-
-    this.locationsHash = locationDAO.getAllLocations();
-
-    // initialize list of location floors and types
-    floorsAndTypes.put("1", 0);
-    floorsAndTypes.put("2", 1);
-    floorsAndTypes.put("3", 2);
-    floorsAndTypes.put("4", 3);
-    floorsAndTypes.put("5", 4);
-    floorsAndTypes.put("1", 0);
-    floorsAndTypes.put("2", 1);
-    floorsAndTypes.put("3", 2);
-    floorsAndTypes.put("L1", 3);
-    floorsAndTypes.put("L2", 4);
-
-    // initialize counts for each type of location to zero
-    for (int i = 0; i < 5; i++) {
-      for (int j = 0; j < 5; j++) {
-        typeCounts[i][j] = 0;
-      }
-    }
-  }
-
-  void deleteNode(String nodeID) {
-    nodes.remove(nodeID);
   }
 
   // gets all locations from the DB and creates nodes from them
@@ -52,29 +21,14 @@ public class RequestNodeManager extends NodeManager {
     requestDAO.csvRead();
     ArrayList<Request> requestsList = new ArrayList<>(requestDAO.getAllRequests().values());
     for (Request request : requestsList) {
-      System.out.println(request.getName());
-      // typeCounts[floorsAndTypes.get(Integer.toString(request.getRequestType()))][
-      //         floorsAndTypes.get(locationsHash.get(request.getLocation()))] +=
-      //     1;
-      mapsController.displayNode(addNode(request));
+      mapsController.displayRequestNode(addNode(request));
     }
-  }
-
-  Integer getTypeCount(String type, String floor) {
-    typeCounts[floorsAndTypes.get(type)][floorsAndTypes.get(floor)] += 1;
-    return typeCounts[floorsAndTypes.get(type)][floorsAndTypes.get(floor)];
   }
 
   RequestNode addNode(Request request) {
     RequestNode requestNode = new RequestNode(request, this);
     nodes.put(requestNode.getName(), requestNode);
     return requestNode;
-    // add the new location to the database
-  }
-
-  void editNode(LocationNode locationNode) {
-    // edit the corresponding location in the backend
-
   }
 
   // gets called on button press and gets the node data
