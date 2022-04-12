54f8ed18b570adac630fed0fc6230c630fa6e50f
Talia Andrews <tgandrews@wpi.edu>
2022-04-12 12:14:51 -0400

    Attempting to implement client-server connection.

--- src/main/java/edu/wpi/agileAngels/Database/DBconnection.java
+++ src/main/java/edu/wpi/agileAngels/Database/DBconnection.java
@@ -4,20 +4,47 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 // Singleton connection
 // One connection, will not create multiple, eliminates repetitive code
 // TODO make sure connections are good, most important part
 public class DBconnection {
+  private static DBConnectionEnum database;
   private static Connection connection;
 
+  //Embedded is the default connection
   static {
     try {
+      database = DBConnectionEnum.EMBEDDED;
       connection = DriverManager.getConnection("jdbc:derby:myDB;create=true");
 
     } catch (SQLException e) {
+      System.out.println("Establishing embedded connection failed.");
       e.printStackTrace();
     }
   }
+
+  //Switching from current connection to the other connection
+  public static void switchConnection(){
+    try {
+      switch (database) {
+        case EMBEDDED:
+          connection.close();
+          connection = DriverManager.getConnection("jdbc:derby://localhost:1527/myCSDB;create=true");
+          break;
+        case CLIENT_SERVER:
+          connection.close();
+          connection = DriverManager.getConnection("jdbc:derby:myDB;create=true");
+          break;
+        default:
+          return;
+      }
+    }catch(SQLException sqlException){
+      System.out.println("Switching the connection failed.");
+      //sqlException.printStackTrace();
+    }
+  }
+
 
   public static Connection getConnection() {
     return connection;
