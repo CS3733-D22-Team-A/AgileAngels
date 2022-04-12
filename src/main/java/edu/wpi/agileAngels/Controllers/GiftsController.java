608d312bb110990004f4221204b2e0439520f632
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2022-04-12 16:05:49 -0400

    more things for gift
    im unsure if the Gift class and DAO interfaces are nessosary

--- src/main/java/edu/wpi/agileAngels/Controllers/GiftsController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/GiftsController.java
@@ -69,31 +69,71 @@ public class GiftsController extends MainController implements Initializable {
 
     giftTable.setItems(giftData);
   }
-  /*
-      @FXML
-    //   Submits fields to a Java gifts Request Object
-      private void submitGift() {
-        String dropDown = dropdownButtonText.getText();
-        String sender = giftSender.getText();
-        String recipient = giftRecipient.getText();
-        String employee = giftEmployeeText.getText();
-        String location = giftLocation.getText();
-        String message = giftMessage.getText();
-        String delete = deleteName.getText();
-        String edit = editRequest.getText();
-        String status = giftStatus.getText();
 
-        // if the fields are empty or to delete input is not empty
-        if (!delete.isEmpty()) {
-          deleteGiftRequest(delete);
-          // editing a request
-        } else if (!edit.isEmpty()) {
-          editGiftRequest(edit, dropDown, "location", employee, status, sender, recipient);
-        } else {
-          addGiftRequest("available", dropDown, location, employee, status, message, sender, recipient);
-        }
-      }
-  */
+  @FXML
+  /** Submits fields to a Java gifts Request Object */
+  private void submitGift() {
+    String dropDown = dropdownButtonText.getText();
+    String sender = giftSender.getText();
+    String recipient = giftRecipient.getText();
+    String employee = giftEmployeeText.getText();
+    // String location = giftLocation.getText();
+    String message = giftMessage.getText();
+    // String delete = deleteName.getText();
+    // String edit = editRequest.getText();
+    String status = giftStatus.getText();
+    // attributes arent all filled
+    if (giftSender.getText().isEmpty()
+        || employee.isEmpty()
+        || dropDown.isEmpty()
+        || recipient.isEmpty()) {
+      giftConfirm.setText("Please fill out all of the required fields");
+    } else {
+      giftConfirm.setText(
+          "Thank you, "
+              + giftSender.getText()
+              + ", "
+              + giftEmployeeText.getText()
+              + " will deliver "
+              + dropdownButtonText.getText()
+              + " to "
+              + giftRecipient.getText()
+              + " soon. ");
+    }
+    addGiftRequest(dropDown, sender, recipient, employee, "location", message, status);
+  }
+
+  private void addGiftRequest(
+      String dropDown,
+      String sender,
+      String recipient,
+      String employee,
+      String location,
+      String message,
+      String status) {
+
+    giftConfirm.setText(
+        "Thank you, "
+            + sender
+            + ", "
+            + employee
+            + " will deliver "
+            + dropDown
+            + " to "
+            + recipient
+            // + " to "
+            // + location
+            + " soon. ");
+
+    String placeholder = "?";
+    Request gift =
+        new Request(placeholder, employee, location, dropDown, status, message, sender, recipient);
+    // todo is this right?
+    GiftrequestImpl.addRequest(gift); // add to hashmap
+    giftData.add(gift); // add to the UI
+    giftTable.setItems(giftData);
+  }
+
   @FXML
   private void deleteGiftRequest(String deleteString) {
     if (!deleteString.isEmpty()) {
@@ -151,129 +191,5 @@ public class GiftsController extends MainController implements Initializable {
 
       giftTable.setItems(giftData);
     }
-  }
-
-  /*
-
-    private void addGiftRequest(
-        String name,
-        String employee,
-        String location,
-        String type,
-        String status,
-        String description,
-        String sender,
-        String recipient) {
-      if (sender.isEmpty()
-          || employee.isEmpty()
-          || type.isEmpty()
-          || location.isEmpty()
-          || recipient.isEmpty()) { // TODO do we need a gift recipient???
-        giftConfirm.setText("Please fill out all of the required fields");
-      } else {
-        giftConfirm.setText(
-            "Thank you, "
-                + sender
-                + ", "
-                + employee
-                + " will deliver "
-                + type
-                + " to "
-                + recipient
-                + " soon. ");
-        GiftRequest request =
-            new GiftRequest("", employee, location, type, status, description, sender, recipient);
-
-        giftDAO.addRequest(request);
-      }
-    }
-
-    @FXML
-    private void clearPage() throws IOException, InterruptedException {
-      loadPage("views/gifts-view.fxml", giftConfirm);
-    }
-  }
-
-
-  */
-
-  // @Override
-  // public void initialize(URL location, ResourceBundle resources) {
-  //  HashMap<String, Request> giftData = new HashMap<>();
-  /**
-   * try { giftDAO = new RequestDAOImpl("./GIFT.CSV", giftData, 0); } catch (SQLException e) {
-   * e.printStackTrace(); }*
-   */
-  // }
-
-  @FXML
-  /** Submits fields to a Java gifts Request Object */
-  private void submitGift() {
-    String dropDown = dropdownButtonText.getText();
-    String sender = giftSender.getText();
-    String recipient = giftRecipient.getText();
-    String employee = giftEmployeeText.getText();
-    // String location = giftLocation.getText();
-    String message = giftMessage.getText();
-    // String delete = deleteName.getText();
-    // String edit = editRequest.getText();
-    String status = giftStatus.getText();
-    // attributes arent all filled
-    if (giftSender.getText().isEmpty()
-        || employee.isEmpty()
-        || dropDown.isEmpty()
-        || recipient.isEmpty()) {
-      giftConfirm.setText("Please fill out all of the required fields");
-    } else {
-      giftConfirm.setText(
-          "Thank you, "
-              + giftSender.getText()
-              + ", "
-              + giftEmployeeText.getText()
-              + " will deliver "
-              + dropdownButtonText.getText()
-              + " to "
-              + giftRecipient.getText()
-              + " soon. ");
-      /**
-       * GiftRequest request = new GiftRequest( "", giftEmployeeText.getText(),
-       * giftRecipient.getText(), dropdownButtonText.getText(), giftStatus.getText(),
-       * giftMessage.getText(), giftSender.getText());
-       *
-       * <p>giftDAO.addRequest(request);*
-       */
-    }
-    addGiftRequest(dropDown, sender, recipient, employee, "location", message, status);
-  }
-
-  private void addGiftRequest(
-      String dropDown,
-      String sender,
-      String recipient,
-      String employee,
-      String location,
-      String message,
-      String status) {
-
-    giftConfirm.setText(
-        "Thank you, "
-            + sender
-            + ", "
-            + employee
-            + " will deliver "
-            + dropDown
-            + " to "
-            + recipient
-            // + " to "
-            // + location
-            + " soon. ");
-
-    String placeholder = "?";
-    Request gift =
-        new Request(placeholder, employee, location, dropDown, status, message, sender, recipient);
-    // todo is this right?
-    GiftrequestImpl.addRequest(gift); // add to hashmap
-    giftData.add(gift); // add to the UI
-    giftTable.setItems(giftData);
   }
 }
