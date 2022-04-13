9237b6216708bc3a301f00ff0fd4bdb25b8f2489
hlarrabee <harmonilarrabee@gmail.com>
2022-04-13 01:49:50 -0400

    finished emergency page

--- src/main/java/edu/wpi/agileAngels/Controllers/EmergencyController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/EmergencyController.java
@@ -1,3 +1,16 @@
 package edu.wpi.agileAngels.Controllers;
 
-public class EmergencyController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class EmergencyController {
+
+  @FXML Button helpButton;
+  @FXML Label helpMessage;
+
+  @FXML
+  public void displayHelpMessage() {
+    helpMessage.setText("Help is on the way to your location.");
+  }
+}
