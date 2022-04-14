7c527c444b8155cbdaed04879e4d960074806143
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2022-04-13 20:51:50 -0400

    re-added the search classes in newer build

--- src/main/java/edu/wpi/agileAngels/Controllers/TestController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/TestController.java
@@ -3,6 +3,7 @@ package edu.wpi.agileAngels.Controllers;
 import edu.wpi.agileAngels.Database.DBconnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import edu.wpi.agileAngels.itemsifters.ItemSearch;
 
 // test button on the front end
 public class TestController extends MainController {
---
+++ src/main/java/edu/wpi/agileAngels/itemsifters/displays/SelectionListDisplay.java
@@ -0,0 +1,65 @@
+package edu.wpi.agileAngels.itemsifters.displays;
+
+import com.jfoenix.controls.JFXListView;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * This is a display that maintains selected items, but keeping a list and sticking whether or not
+ * it has been selected into its cell creator as a boolean. The benefit of this is you can maintain
+ * selections in between updates. This is totally unnecessary, because your cellCreator, and any
+ * filters can just be defined in the scope of your "selections" list, meaning you can refer to it
+ * similar to how this thing does it.
+ */
+public class SelectionListDisplay<Item, Cell> extends ListViewDisplay<Item, Cell> {
+  SelectionCellCreator<Item, Cell> selectionCellCreator;
+  List<Item> selectedItems;
+
+  public interface SelectionCellCreator<Item, Cell> {
+    Cell makeCell(Item item, boolean selected);
+  }
+
+  public SelectionListDisplay(
+      JFXListView<Cell> listView, SelectionCellCreator<Item, Cell> selectionCellCreator) {
+    this.selectedItems = new LinkedList<>();
+    this.selectionCellCreator = selectionCellCreator;
+    this.listView = listView;
+  }
+
+  @Override
+  protected void populateListView(Stream<Item> items) {
+    selectedItems.forEach(
+        selectedItem -> {
+          Cell cell = selectionCellCreator.makeCell(selectedItem, true);
+          listView.getItems().add(cell);
+        });
+    items
+        .filter(item -> !selectedItems.contains(item))
+        .forEach(
+            item -> {
+              Cell cell = selectionCellCreator.makeCell(item, false);
+              listView.getItems().add(cell);
+            });
+  }
+
+  public void selectItem(Item item) {
+    selectedItems.add(item);
+  }
+
+  public void deSelectItem(Item item) {
+    selectedItems.remove(item);
+  }
+
+  public void clearSelectedItems() {
+    selectedItems.clear();
+  }
+
+  public void setSelectionCellCreator(SelectionCellCreator<Item, Cell> selectionCellCreator) {
+    this.selectionCellCreator = selectionCellCreator;
+  }
+
+  public List<Item> getSelectedItems() {
+    return selectedItems;
+  }
+}
