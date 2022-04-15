4304274576b1902d1ab2f3cdbd5ade9b5027de9f
InDanity <101886886+InDanity@users.noreply.github.com>
2022-04-14 20:39:00 -0400

    Added MethodTestClass to Git. :D

---
+++ src/main/java/edu/wpi/agileAngels/Controllers/MethodTestClass.java
@@ -0,0 +1,26 @@
+package edu.wpi.agileAngels.Controllers;
+
+import edu.wpi.agileAngels.Database.Employee;
+import edu.wpi.agileAngels.Database.Request;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class MethodTestClass {
+  int timesDanLaughs;
+
+  public MethodTestClass(int timesDanLaughs) {
+    this.timesDanLaughs = timesDanLaughs;
+  }
+
+  public ObservableList<Request> filterReqEmployeeNoMedData(
+      Employee employee, ObservableList<Request> listChecked) {
+    ObservableList<Request> newList = FXCollections.observableArrayList();
+
+    for (Request req : listChecked) {
+      if (req.getEmployee().equals(employee)) {
+        newList.add(req);
+      }
+    }
+    return newList;
+  }
+}
