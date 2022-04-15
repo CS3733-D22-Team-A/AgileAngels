b7c73ad8dd910731f6e8aa3ee705cf62e9b37e0c
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2022-04-14 23:41:58 -0400

    Added code, just having import issues

--- src/main/java/edu/wpi/agileAngels/Controllers/TestController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/TestController.java
@@ -1,15 +1,30 @@
 package edu.wpi.agileAngels.Controllers;
 
 import edu.wpi.agileAngels.Database.DBconnection;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import edu.wpi.agileAngels.itemsifters.ItemSearch;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import org.controlsfx.control.textfield.TextFields;
 
 // test button on the front end
-public class TestController extends MainController {
+public class TestController extends MainController implements Initializable {
+
+  @FXML private TextField input;
+
+  @FXML private AnchorPane main;
 
   @FXML
   public void switchConnection(ActionEvent event) {
     DBconnection.switchConnection();
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    String[] words = {"Test", "This", "Text"};
+    TextFields.bindAutoCompletion(input, words);
   }
 }
