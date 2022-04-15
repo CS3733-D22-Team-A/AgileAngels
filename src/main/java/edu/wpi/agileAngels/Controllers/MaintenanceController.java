08e237843e32a7a348ad2cc5d6aede913d36c394
Talia Andrews <tgandrews@wpi.edu>
2022-04-14 22:33:54 -0400

    Rectangle!

--- src/main/java/edu/wpi/agileAngels/Controllers/MaintenanceController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/MaintenanceController.java
@@ -1,3 +1,11 @@
 package edu.wpi.agileAngels.Controllers;
 
-public class MaintenanceController {}
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class MaintenanceController implements Initializable {
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
+}
