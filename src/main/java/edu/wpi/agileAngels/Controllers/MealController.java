1e78f6dfe76a0db61585269ad4591dbcf28ce8f3
Talia Andrews <tgandrews@wpi.edu>
2022-04-18 23:43:08 -0400

    changes to meal request ui and beginning of backend functionality

--- src/main/java/edu/wpi/agileAngels/Controllers/MealController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/MealController.java
@@ -5,38 +5,58 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.ResourceBundle;
+import java.util.*;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
 public class MealController implements Initializable, PropertyChangeListener {
-  AppController appController = AppController.getInstance();
-  @FXML private Button mealDropdown, pasta, steak, burger, pizza;
-  @FXML private TextField roomInput, mealEmployeeText, mealStatus, restrictions;
-  @FXML private Label dropdownButtonText, confirm, pastaLabel, stealLabel, burgerLabel, pizzaLabel;
-  @FXML Pane drop, drop2;
 
-  private RequestDAOImpl mealDAO;
-  private ObservableList mealList = FXCollections.observableArrayList();
-  LocationDAOImpl locDAO = LocationDAOImpl.getInstance();
+  @FXML Pane popOut;
+  @FXML
+  MenuButton mealID, mealLocation, mealEmployee, mealStatus;
+  @FXML Button modifyButton, cancelRequest, submitRequest, clearRequest, deleteRequest;
+  @FXML
+  TableView mealTable;
+  @FXML
+  private TableColumn nameColumn,
+          availableColumn,
+          typeColumn,
+          locationColumn,
+          employeeColumn,
+          statusColumn,
+          descriptionColumn;
+  @FXML TextField mealDescription, employeeFilterField, statusFilterField;
+  @FXML Label notStartedNumber, inProgressNumber, completedNumber;
+
+  private LocationDAOImpl locDAO = LocationDAOImpl.getInstance();
   private EmployeeManager empDAO = EmployeeManager.getInstance();
+  private RequestDAOImpl mealRequestImpl = RequestDAOImpl.getInstance("MealRequest");
+  private HashMap<String, Location> locationsHash = locDAO.getAllLocations();
+  private ArrayList<Location> locationsList = new ArrayList<>(locationsHash.values());
+  private HashMap<String, Employee> employeeHash = empDAO.getAllEmployees();
+  private static ObservableList<Request> mealData = FXCollections.observableArrayList();
+  AppController appController = AppController.getInstance();
+
+  private int statusNotStarted, statusInProgress, statusComplete;
 
   public MealController() throws SQLException {}
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     appController.addPropertyChangeListener(this);
+    appController.addPropertyChangeListener(this);
+    popOut.setVisible(false);
+    statusNotStarted = 0;
+    statusInProgress = 0;
+    statusComplete = 0;
 
-    HashMap<String, Request> mealData = new HashMap<String, Request>();
-    // mealDAO = new RequestDAOImpl("./Meal.csv", mealData, 0);
+
   }
 
   @Override
@@ -47,48 +67,224 @@ public class MealController implements Initializable, PropertyChangeListener {
   }
 
   @FXML
-  private void submitMeal() {
-    String dropDown = dropdownButtonText.getText();
-    String location = roomInput.getText();
-    Location loc = locDAO.getLocation(location);
-    String employee = mealEmployeeText.getText();
-    Employee emp = empDAO.getEmployee(employee);
-    String status = mealStatus.getText();
-    // String restrictions = restrictions.getText();
-    if (dropdownButtonText.getText().isEmpty()
-        || roomInput.getText().isEmpty()
-        || dropdownButtonText.getText().isEmpty()) {
-      confirm.setText("Please fill out all the required fields");
-    } else {
-      confirm.setText(
-          "Your order "
-              + dropDown
-              + " will be delivered by "
-              + emp
-              + " to room "
-              + loc
-              + ". Special Instructions: "
-              + restrictions.getText());
+  public void modifyRequest(ActionEvent event) {
 
-      Request request =
-          new Request(
-              "",
-              empDAO.getEmployee(mealEmployeeText.getText()),
-              locDAO.getLocation(roomInput.getText()),
-              dropdownButtonText.getText(),
-              mealStatus.getText(),
-              restrictions.getText(),
-              "",
-              "");
-      mealDAO.addRequest(request);
-      mealList.add(request);
-      // add table
+  }
+
+  @FXML
+  public void submit(ActionEvent event) {
+
+  }
+
+  @FXML
+  public void cancel(ActionEvent event) {
+
+  }
+
+  @FXML
+  public void delete(ActionEvent event) {
+
+  }
+
+  @FXML
+  public void clear(ActionEvent event) {
+
+  }
+
+  @FXML
+  public void mealIDMenu(ActionEvent event) {
+
+  }
+
+  @FXML
+  public void mealLocationMenu(ActionEvent event) {
+
+  }
+
+  @FXML
+  public void mealEmployeeMenu(ActionEvent event) {
+
+  }
+
+  @FXML
+  public void mealStatusMenu(ActionEvent event) {
+
+  }
+
+  @FXML
+  public void filterReqOnAction(ActionEvent event) {
+    if (!employeeFilterField.getText().isEmpty() && !statusFilterField.getText().isEmpty()) {
+
+      ObservableList<Request> empFilteredList = filterReqEmployee(employeeFilterField.getText());
+      ObservableList<Request> trueFilteredList =
+              filterFilteredReqListStatus(statusFilterField.getText(), empFilteredList);
+
+      mealTable.setItems(trueFilteredList);
+    } else if (!employeeFilterField.getText().isEmpty()) {
+      filterReqsTableEmployee(employeeFilterField.getText());
+    } else if (!statusFilterField.getText().isEmpty()) {
+      filterReqsTableStatus(statusFilterField.getText());
     }
   }
 
-  private void addMealRequest() {}
+  @FXML
+  public void clearFilters(ActionEvent event) {
+    mealTable.setItems(mealData);
+  }
 
-  public void clearPage(ActionEvent actionEvent) {
-    appController.clearPage();
+  /**
+   * This is the cleaner version of Justin's dashboard code. Note that it may need a for loop as
+   * shown on line 83/84 if used elsewhere. Note: Unlikely.
+   */
+  @FXML
+  private void dashboardLoad() {
+    if (notStartedNumber.getText().equals("-")
+            && inProgressNumber.getText().equals("-")
+            && completedNumber.getText().equals("-")) {
+
+      Iterator var3 = mealRequestImpl.getAllRequests().entrySet().iterator();
+      while (var3.hasNext()) {
+        Map.Entry<String, Request> entry = (Map.Entry) var3.next();
+        Request object = (Request) entry.getValue();
+        if (entry.getValue().getStatus().equals("inProgress")) {
+          statusInProgress++;
+        }
+        if (entry.getValue().getStatus().equals("notStarted")) {
+          statusNotStarted++;
+        }
+        if (entry.getValue().getStatus().equals("Complete")
+                || entry.getValue().getStatus().equals("complete")) {
+          statusComplete++;
+        }
+      }
+      setDashboard(statusNotStarted, statusInProgress, statusComplete);
+    }
+  }
+
+  /**
+   * Will set the dashboard's numbers to the certain types of statuses.
+   *
+   * @param notStarted Requests not started
+   * @param inProgress Requests in progress
+   * @param complete Requests completed
+   */
+  @FXML
+  private void setDashboard(int notStarted, int inProgress, int complete) {
+    String notStart = Integer.toString(notStarted);
+    String inProg = Integer.toString(inProgress);
+    String comp = Integer.toString(complete);
+    notStartedNumber.setText(notStart);
+    inProgressNumber.setText(inProg);
+    completedNumber.setText(comp);
+  }
+
+  /**
+   * Filters requests in the equipment table so only those with the given Employee remain.
+   *
+   * @param employeeName The Employee the requests must have to remain on the table.
+   */
+  private void filterReqsTableEmployee(String employeeName) {
+    ObservableList<Request> filteredList = filterReqEmployee(employeeName);
+
+    // Sets table to only have contents of the filtered list.
+    mealTable.setItems(filteredList);
+  }
+
+  /**
+   * Filters out requests in labData based on the given Employee.
+   *
+   * @param employeeName The Employee that the requests must have to be in the new list.
+   * @return The new filtered list.
+   */
+  private ObservableList<Request> filterReqEmployee(String employeeName) {
+    ObservableList<Request> newList = FXCollections.observableArrayList();
+
+    for (Request req : mealData) {
+      if (req.getEmployee().getName().equals(employeeName)) {
+        newList.add(req);
+      }
+    }
+
+    return newList;
+  }
+
+  /**
+   * Filters requests in the maintenance table so only those with the given status remain.
+   *
+   * @param reqStatus The status the requests must have to remain on the table.
+   */
+  private void filterReqsTableStatus(String reqStatus) {
+    ObservableList<Request> filteredList = filterReqStatus(reqStatus);
+    // Sets table to only have contents of the filtered list.
+    mealTable.setItems(filteredList);
+  }
+
+  /**
+   * Filters out requests in mainData based on the given status.
+   *
+   * @param reqStatus The status that the requests must have to be in the new list.
+   * @return The new filtered list.
+   */
+  private ObservableList<Request> filterReqStatus(String reqStatus) {
+    ObservableList<Request> newList = FXCollections.observableArrayList();
+
+    for (Request req : mealData) {
+      if (req.getStatus().equals(reqStatus)) {
+        newList.add(req);
+      }
+    }
+    return newList;
+  }
+
+  /**
+   * Filters out requests in mainData based on the given status.
+   *
+   * @param reqStatus The status that the requests must have to be in the new list.
+   * @param filteredList The list that was presumably filtered.
+   * @return The new filtered list.
+   */
+  private ObservableList<Request> filterFilteredReqListStatus(
+          String reqStatus, ObservableList<Request> filteredList) {
+    ObservableList<Request> newList = FXCollections.observableArrayList();
+
+    for (Request req : filteredList) {
+      if (req.getStatus().equals(reqStatus)) {
+        newList.add(req);
+      }
+    }
+    return newList;
+  }
+
+  /**
+   * Filters out requests in mainData based on the given Employee.
+   *
+   * @param employeeName The Employee that the requests must have to be in the new list.
+   * @param filteredList The list that was presumably filtered.
+   * @return The new filtered list.
+   */
+  private ObservableList<Request> filterFilteredReqListEmployee(
+          String employeeName, ObservableList<Request> filteredList) {
+    ObservableList<Request> newList = FXCollections.observableArrayList();
+
+    for (Request req : filteredList) {
+      if (req.getEmployee().getName().equals(employeeName)) {
+        newList.add(req);
+      }
+    }
+
+    return newList;
+  }
+
+  /**
+   * Populates fields once a node id is chosen when editing an existing request.
+   *
+   * @param id Request ID
+   */
+  private void populate(String id) {
+    Request req = mealRequestImpl.getAllRequests().get(id);
+    mealLocation.setText(req.getLocation().getNodeID());
+    mealEmployee.setText(req.getEmployee().getName());
+    mealStatus.setText(req.getStatus());
+    mealDescription.setText(req.getDescription());
   }
 }
