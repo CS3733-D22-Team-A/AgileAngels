e1f537b846a732902c7e78decd127f57d7b19472
physicalfrog4 <77911997+physicalfrog4@users.noreply.github.com>
2022-04-21 14:08:08 -0400

    got page to open and first view edits ;~)

---
+++ src/main/java/edu/wpi/agileAngels/Controllers/MedAidController.java
@@ -0,0 +1,11 @@
+package edu.wpi.agileAngels.Controllers;
+
+import javafx.event.ActionEvent;
+
+public class MedAidController {
+  private void MedAidViewController() {}
+
+  public void submit(ActionEvent actionEvent) {}
+
+  public void clear(ActionEvent actionEvent) {}
+}
