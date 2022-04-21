aff9371548ed20eafe93bb159d9af951c8eed461
physicalfrog4 <77911997+physicalfrog4@users.noreply.github.com>
2022-04-21 14:49:02 -0400

    making a CSV file to parse from

--- src/main/java/edu/wpi/agileAngels/Controllers/MedAidController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/MedAidController.java
@@ -1,11 +1,35 @@
 package edu.wpi.agileAngels.Controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
 
-public class MedAidController {
+public class MedAidController implements Initializable {
+  public Pane treatment1, treatment2, treatment3;
+  AppController appController = AppController.getInstance();
+
   private void MedAidViewController() {}
 
-  public void submit(ActionEvent actionEvent) {}
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
 
-  public void clear(ActionEvent actionEvent) {}
+    // appController.addPropertyChangeListener(this);
+    treatment1.setVisible(false);
+    treatment2.setVisible(false);
+    treatment3.setVisible(false);
+  }
+
+  public void submit(ActionEvent actionEvent) {
+    treatment1.setVisible(true);
+    treatment2.setVisible(true);
+    treatment3.setVisible(true);
+  }
+
+  public void clear(ActionEvent actionEvent) {
+    treatment1.setVisible(false);
+    treatment2.setVisible(false);
+    treatment3.setVisible(false);
+  }
 }
