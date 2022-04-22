b79e5507ba399ba8d4b774a47cab32f45ff33ce9
InDanity <101886886+InDanity@users.noreply.github.com>
2022-04-21 22:08:51 -0400

    Added page and controller FOR REAL. :)

---
+++ src/main/java/edu/wpi/agileAngels/Controllers/APILandController.java
@@ -0,0 +1,12 @@
+package edu.wpi.agileAngels.Controllers;
+
+import java.beans.PropertyChangeListener;
+import java.sql.SQLException;
+
+public class APILandController {
+
+    AppController appController = AppController.getInstance();
+
+    public APILandController() throws SQLException {}
+
+}
