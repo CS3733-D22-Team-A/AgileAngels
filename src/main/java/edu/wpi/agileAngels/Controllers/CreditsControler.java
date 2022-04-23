7e264a6ae941bce2f51892592cb0291d661ee314
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2022-04-22 20:08:00 -0400

    more changes to prep for finilization

--- src/main/java/edu/wpi/agileAngels/Controllers/CreditsControler.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/CreditsControler.java
@@ -1,4 +1,27 @@
 package edu.wpi.agileAngels.Controllers;
 
-public class CreditsControler {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreditsControler implements Initializable, PropertyChangeListener {
+
+
+    @FXML
+    Button apiButton12, apiButton1, creditsButton;
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
