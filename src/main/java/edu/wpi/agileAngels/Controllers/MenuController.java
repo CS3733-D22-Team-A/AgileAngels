c850a8f6c2b18d8b0bf6d9a0da80fc9b3b5aaafa
Jaffyspark11 <josephashtonfox@gmail.com>
2022-04-23 13:23:18 -0400

    no more home view button on menu bar goes to dashboard now

--- src/main/java/edu/wpi/agileAngels/Controllers/MenuController.java
+++ src/main/java/edu/wpi/agileAngels/Controllers/MenuController.java
@@ -77,7 +77,7 @@ public class MenuController implements Initializable {
   public void profile(ActionEvent event) {}
 
   public void goHome(ActionEvent event) {
-    appController.loadPage("/edu/wpi/agileAngels/views/home-view.fxml");
+    appController.loadPage("/edu/wpi/agileAngels/views/NEWdashboard.fxml");
   }
 
   public void setUserInitials(String initialsMaker) {
